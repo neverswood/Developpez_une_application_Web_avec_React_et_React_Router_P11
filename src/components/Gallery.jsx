148227b44161a8897738a311c6a56849257c48d6
neverswood <nevers_wood@hotmail.fr>
2022-06-10 09:29:12 +0200

    Create gallery lodging's card for the homepage

---
+++ src/components/Gallery.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+//import data from '../data/data.json';
+import '../styles/Gallery.css';
+import Card from './Card';
+
+function Gallery() {
+  return (
+    <div className="kasa-gallery">
+      <Card />
+    </div>
+  );
+}
+
+export default Gallery;
