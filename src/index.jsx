29da99864f79ad3faada37e0205a7d3274fec1ff
neverswood <nevers_wood@hotmail.fr>
2022-06-10 09:39:12 +0200

    Add apropos page, footer and path for home

--- src/index.jsx
+++ src/index.jsx
@@ -1,18 +1,25 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Footer from './components/Footer';
+import Header from './components/Header';
+import Apropos from './pages/APropos';
 import Home from './pages/Home';
+import './styles/Nomavoir.css';
 
 const container = document.getElementById('root');
 const root = createRoot(container); // createRoot(container!) if you use TypeScript
 root.render(
   <React.StrictMode>
     <Router>
+      <Header />
       <Routes>
         <Route path="/">
-          <Route index element={<Home />} />
+          <Route path="Home" index element={<Home />} />
+          <Route path="Apropos" element={<Apropos />} />
         </Route>
       </Routes>
+      <Footer />
     </Router>
   </React.StrictMode>
 );
