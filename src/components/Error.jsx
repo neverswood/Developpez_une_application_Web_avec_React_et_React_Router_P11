34d76f298daba524deed1fb7258999ab22dc0aaa
neverswood <nevers_wood@hotmail.fr>
2022-06-15 12:55:45 +0200

    Create the html and the style for the error that is sent when the url does not exist

--- src/components/Error.jsx
+++ src/components/Error.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
+import '../styles/Error.css';
 
 export function Error() {
   return (
-    <div>
+    <main className="error-page">
       <h1>404</h1>
-    </div>
+      <p>Oups! La page que vous demandez n'existe pas.</p>
+      <Link to="/home">Retourner sur la page d'acceuil</Link>
+    </main>
   );
 }
