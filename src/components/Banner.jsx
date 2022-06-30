d503e8f5722ac10bff34f95e4f1f0da8c080de44
neverswood <nevers_wood@hotmail.fr>
2022-06-30 14:09:40 +0200

    Change export and semantic balise for the component banner

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -2,13 +2,11 @@ import React from 'react';
 import banner from '../assets/banner.png';
 import '../styles/Banner.css';
 
-function Banner() {
+export function Banner() {
   return (
-    <div className="kasa-banner">
+    <section className="kasa-banner">
       <img src={banner} alt="banner" className="kasa-banner__img" />
       <h1 className="kasa-banner__slogan">Chez vous, partout et ailleurs</h1>
-    </div>
+    </section>
   );
 }
-
-export default Banner;
