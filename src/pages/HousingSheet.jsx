a95983eb07a851de89ed58ef3c0a2150b0307e9a
neverswood <nevers_wood@hotmail.fr>
2022-06-30 14:23:36 +0200

    Add accordion and add classname to the housingSheet page

--- src/pages/HousingSheet.jsx
+++ src/pages/HousingSheet.jsx
@@ -2,17 +2,36 @@ import React from 'react';
 import { useParams } from 'react-router-dom';
 import { Gallery } from '../components/Gallery';
 import { LodgingPresentation } from '../components/LodgingPresentation';
+import { Accordion } from '../components/Accordion';
+import '../styles/HousingSheet.css';
 import data from '../data/data.json';
 
 export function HousingSheet() {
   const params = useParams();
   const paramsId = params.id;
   const lodging = data.find((lodgingId) => lodgingId.id === paramsId);
-  console.log(lodging);
   return (
-    <div>
+    <div className="housingSheet">
       <Gallery pictures={lodging.pictures} />
       <LodgingPresentation lodging={lodging} />
+      <Accordion
+        title="Description"
+        content={
+          <div className="description">
+            <p className="descriptionP">{`${lodging.description}`}</p>
+          </div>
+        }
+      />
+      <Accordion
+        title="Équipements"
+        content={
+          <ul>
+            {lodging.equipments.map((equipment) => {
+              return <li>{equipment}</li>;
+            })}
+          </ul>
+        }
+      />
     </div>
   );
 }
