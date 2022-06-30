d711150a8e87834e58a14855c9df5a35df3d2445
neverswood <nevers_wood@hotmail.fr>
2022-06-30 14:11:52 +0200

    Change export and add link to access the page of the selected accomodation

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,21 +1,22 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import data from '../data/data.json';
 import '../styles/Card.css';
 
-function Card() {
+export function Card() {
   const lodging = data.map((toto) => {
     return (
-      <div className="kasa-card">
-        <img
-          src={toto.cover}
-          alt={`cover-{toto.id}`}
-          className="kasa-card__cover"
-        />
-        <h2 className="kasa-card__title">{toto.title}</h2>
+      <div className="kasa-card" key={toto.id}>
+        <Link to={`/housingSheet/${toto.id}`}>
+          <img
+            src={toto.cover}
+            alt="lodging-pictureCover"
+            className="kasa-card__cover"
+          />
+          <h2 className="kasa-card__title">{toto.title}</h2>
+        </Link>
       </div>
     );
   });
   return <div className="kasa-lodging">{lodging}</div>;
 }
-
-export default Card;
