f8319523a2de9ae974febfff48abebac3d04e536
neverswood <nevers_wood@hotmail.fr>
2022-06-30 14:15:27 +0200

    Create html for the gallery and create button with function for change picture in the carousel gallery

---
+++ src/components/Gallery.jsx
@@ -0,0 +1,49 @@
+import React, { useState } from 'react';
+import '../styles/Gallery.css';
+
+export function Gallery(props) {
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const changeRight = () => {
+    if (currentIndex < props.pictures.length - 1) {
+      setCurrentIndex(currentIndex + 1);
+    } else {
+      setCurrentIndex(0);
+    }
+    return;
+  };
+
+  const changeLeft = () => {
+    if (currentIndex === 0) {
+      setCurrentIndex(props.pictures.length - 1);
+    } else {
+      setCurrentIndex(currentIndex - 1);
+    }
+    return;
+  };
+
+  return (
+    <div className="gallery">
+      <button
+        className="btn-gallery btn-left"
+        onClick={() => {
+          changeLeft();
+        }}
+      >
+        <i className="arrow arrow-left"></i>
+      </button>
+      <img
+        src={props.pictures[currentIndex]}
+        alt="lodgingPicture"
+        className="carousel"
+      />
+      <button
+        className="btn-gallery btn-right"
+        onClick={() => {
+          changeRight();
+        }}
+      >
+        <i className="arrow arrow-right"></i>
+      </button>
+    </div>
+  );
+}
