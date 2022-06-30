afe75093f548ad83913a307b30a255dc2a2f622b
neverswood <nevers_wood@hotmail.fr>
2022-06-30 14:21:13 +0200

    Change the html to about page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,9 +1,57 @@
 import React from 'react';
+import { Accordion } from '../components/Accordion';
+import '../styles/About.css';
+import '../styles/Accordion.css';
 
 export function About() {
   return (
-    <div>
-      <p>a propos</p>
+    <div className="about">
+      <div className="imgBanner"></div>
+      <section>
+        <Accordion
+          title="Fiabilité"
+          content={
+            <p>
+              Les annonces postées sur Kasa garantissent une fiabilité totale.
+              Les photos sont conformes aux logements, et toutes les
+              informations sont régulièrement vérifiées par nos équipes.
+            </p>
+          }
+        />
+        <Accordion
+          title="Respect"
+          content={
+            <p>
+              La bienveillance fait partie des valeurs fondatrices de Kasa. Tout
+              comportement discriminatoire ou de perturbation du voisinage
+              entraînera une exclusion de notre plateforme.
+            </p>
+          }
+        />
+        <Accordion
+          title="Service"
+          content={
+            <p>
+              Nos équipes se tiennent à votre disposition pour vous fournir une
+              expérience parfaite. N'hésitez pas à nous contacter si vous avez
+              la moindre question.
+            </p>
+          }
+        />
+        <Accordion
+          title="Sécurité"
+          content={
+            <p>
+              La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que
+              pour les voyageurs, chaque logement correspond aux critères de
+              sécurité établis par nos services. En laissant une note aussi bien
+              à l'hôte qu'au locataire, cela permet à nos équipes de vérifier
+              que les standards sont bien respectés. Nous organisons également
+              des ateliers sur la sécurité domestique pour nos hôtes.
+            </p>
+          }
+        />
+      </section>
     </div>
   );
 }
