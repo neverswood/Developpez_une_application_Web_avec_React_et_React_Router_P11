70276eff1fd65ed2b750eef2d92f9c10dd331367
neverswood <nevers_wood@hotmail.fr>
2022-06-30 14:13:02 +0200

    Change tag p to tag img in the footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,13 +1,12 @@
 import React from 'react';
 import '../styles/Footer.css';
 import logoWhite from '../assets/logoWhite.png';
-import kasa2020 from '../assets/kasa2020.png';
 
 export function Footer() {
   return (
     <footer>
       <img src={logoWhite} alt="Kasa" className="kasa-logoWhite" />
-      <img src={kasa2020} alt="Kasa" className="kasa-2020" />
+      <p className="kasa-2020">&copy; 2020 Kasa. All rights reserved</p>
     </footer>
   );
 }
