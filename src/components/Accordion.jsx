50e936568aa254cb01c74eea25ba3db9da31c36a
neverswood <nevers_wood@hotmail.fr>
2022-07-04 14:48:01 +0200

    Change the useState of the collapse to put the collapse closed on initialization

--- src/components/Accordion.jsx
+++ src/components/Accordion.jsx
@@ -2,20 +2,20 @@ import React, { useState } from 'react';
 import '../styles/Accordion.css';
 
 export function Accordion(props) {
-  const [isOpen, setIsOpen] = useState(true);
+  const [isOpen, setIsOpen] = useState(false);
 
   return isOpen ? (
     <div className="accordion">
       <button className="btn" onClick={() => setIsOpen(false)}>
-        {props.title} <i className="fas fa-chevron-down"></i>
+        {props.title} <i className="fas fa-chevron-up"></i>
       </button>
+      {props.content}
     </div>
   ) : (
     <div className="accordion">
       <button className="btn" onClick={() => setIsOpen(true)}>
-        {props.title} <i className="fas fa-chevron-up"></i>
+        {props.title} <i className="fas fa-chevron-down"></i>
       </button>
-      {props.content}
     </div>
   );
 }
