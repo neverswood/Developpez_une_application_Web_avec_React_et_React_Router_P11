eb1dcc300193021ebc537e23ea04918065d6e5a5
neverswood <nevers_wood@hotmail.fr>
2022-07-04 14:50:11 +0200

    Change the onClick in the gallery

---
+++ src/components/Gallery.jsx
@@ -0,0 +1,37 @@
+import React, { useState } from 'react';
+import '../styles/Gallery.css';
+
+export function Gallery(props) {
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const changePictureToRight = () => {
+    if (currentIndex < props.pictures.length - 1) {
+      setCurrentIndex(currentIndex + 1);
+    } else {
+      setCurrentIndex(0);
+    }
+  };
+
+  const changePictureToLeft = () => {
+    if (currentIndex === 0) {
+      setCurrentIndex(props.pictures.length - 1);
+    } else {
+      setCurrentIndex(currentIndex - 1);
+    }
+  };
+
+  return props.pictures.length > 1 ? (
+    <div className="gallery">
+      <button className="btn-gallery btn-left" onClick={changePictureToLeft}>
+        <i className="arrow arrow-left"></i>
+      </button>
+      <img src={props.pictures[currentIndex]} alt={props.title} />
+      <button className="btn-gallery btn-right" onClick={changePictureToRight}>
+        <i className="arrow arrow-right"></i>
+      </button>
+    </div>
+  ) : (
+    <div className="gallery">
+      <img src={props.pictures[currentIndex]} alt={props.title} />
+    </div>
+  );
+}
