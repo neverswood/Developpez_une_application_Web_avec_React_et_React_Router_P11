20c13e9cedde81814d763f922bcdcf03c2796a76
neverswood <nevers_wood@hotmail.fr>
2022-07-07 14:05:58 +0200

    Change of gallery functions and condition

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -6,50 +6,35 @@ export function Gallery(props) {
   const changePictureToRight = () => {
     if (currentIndex < props.pictures.length - 1) {
       setCurrentIndex(currentIndex + 1);
-      document.querySelector('.number-media').innerHTML = `${
-        currentIndex + 2
-      } / ${props.pictures.length}`;
     } else {
       setCurrentIndex(0);
-      document.querySelector(
-        '.number-media'
-      ).innerHTML = `1 / ${props.pictures.length}`;
     }
   };
 
   const changePictureToLeft = () => {
     if (currentIndex === 0) {
       setCurrentIndex(props.pictures.length - 1);
-      document.querySelector(
-        '.number-media'
-      ).innerHTML = `${props.pictures.length}  / ${props.pictures.length}`;
     } else {
       setCurrentIndex(currentIndex - 1);
-      document.querySelector(
-        '.number-media'
-      ).innerHTML = `${currentIndex} / ${props.pictures.length}`;
     }
   };
 
-  return props.pictures.length > 1 ? (
+  return (
     <div className="gallery">
-      <button className="btn-gallery btn-left" onClick={changePictureToLeft}>
-        <i className="arrow arrow-left"></i>
-      </button>
+      {props.pictures.length > 1 && (
+        <button className="btn-gallery btn-left" onClick={changePictureToLeft}>
+          <i className="arrow arrow-left"></i>
+        </button>
+      )}
       <img src={props.pictures[currentIndex]} alt={props.title} />
       <span className="number-media">
         {currentIndex + 1}/{props.pictures.length}
       </span>
       <button className="btn-gallery btn-right" onClick={changePictureToRight}>
-        <i className="arrow arrow-right"></i>
+        {props.pictures.length > 1 ? (
+          <i className="arrow arrow-right"></i>
+        ) : null}
       </button>
-    </div>
-  ) : (
-    <div className="gallery">
-      <img src={props.pictures[currentIndex]} alt={props.title} />
-      <span className="number-media">
-        {currentIndex + 1}/{props.pictures.length}
-      </span>
     </div>
   );
 }
