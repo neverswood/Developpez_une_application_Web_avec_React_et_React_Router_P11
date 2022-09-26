9d1de40304db746817c59d08d7e1d3b6c8e345fb
neverswood <nevers_wood@hotmail.fr>
2022-09-26 14:54:03 +0200

    Add data in lodgingList component in the home component

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Banner } from './Banner.jsx';
 import { LodgingList } from './LodgingList.jsx';
+import data from '../../data/data.json';
 
 export function Home() {
   return (
     <main className="home">
       <Banner />
-      <LodgingList />
+      <LodgingList data={data} />
     </main>
   );
 }
