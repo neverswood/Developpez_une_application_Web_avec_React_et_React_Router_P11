7dab042421f00f5b97b25a692f100ca59de314ee
neverswood <nevers_wood@hotmail.fr>
2022-09-26 14:52:49 +0200

    Change the card component

--- src/pages/Home/Card.jsx
+++ src/pages/Home/Card.jsx
@@ -1,22 +1,18 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import data from '../../data/data.json';
 import './Card.css';
 
-export function Card() {
-  const lodgings = data.map((lodging) => {
-    return (
-      <div className="kasa-card" key={lodging.id}>
-        <Link to={`/lodging-sheet/${lodging.id}`}>
-          <img
-            src={lodging.cover}
-            alt={lodging.title}
-            className="kasa-card__cover"
-          />
-          <h2 className="kasa-card__title">{lodging.title}</h2>
-        </Link>
-      </div>
-    );
-  });
-  return <div className="kasa-lodging">{lodgings}</div>;
+export function Card({ lodging }) {
+  return (
+    <div className="kasa-card">
+      <Link to={`/lodging-sheet/${lodging.id}`}>
+        <img
+          src={lodging.cover}
+          alt={lodging.title}
+          className="kasa-card__cover"
+        />
+        <h2 className="kasa-card__title">{lodging.title}</h2>
+      </Link>
+    </div>
+  );
 }
